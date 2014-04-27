ebd77bfe42aec495f9c090fefc167cd030bae636
David Rozenberg <daveroze@gmail.com>
2014-04-27 17:04:23 +0000

    Changed index.js to use port 8080 and display index.html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+var fs = require('fs');
+var htmlfile = 'index.html';
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html)
 })
 
 app.listen(app.get('port'), function() {
